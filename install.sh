eb7ad9b946c8dd37fed7916e3ffd958ab8086c4e
Zachary Chai <zachary.chai@outlook.com>
2015-10-07 00:08:21 -0400

    install script for docker and compose

---
+++ install.sh
@@ -0,0 +1,3 @@
+wget -qO- https://get.docker.com/ | sh
+curl -L https://github.com/docker/compose/releases/download/1.4.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
