5ee313930d6890c6fbabcdd0e72181a81e3869ce
Zachary Chai <zachary.chai@outlook.com>
2015-10-11 18:44:55 -0400

    update script and make sure to run with sudo

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Script to install the latest docker and compose
 
 Clone the repo ```git clone git@github.com:zach-chai/docker-compose-install.git```
 
-Run the script ```./install_latest.sh```
+Run the script ```sudo ./install_latest.sh```
--- install_latest.sh
+++ install_latest.sh
@@ -1,4 +1,9 @@
-LATEST="$(curl -sL https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d'"' -f4)"
-wget -qO- https://get.docker.com/ | sh
-curl -L https://github.com/docker/compose/releases/download/${LATEST}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+if type "wget" > /dev/null; then
+  LATEST="$(wget -qO- https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d'"' -f4)"
+  wget -qO- https://get.docker.com/ | sh
+  wget -q https://github.com/docker/compose/releases/download/${LATEST}/docker-compose-`uname -s`-`uname -m`
+  mv docker-compose-`uname -s`-`uname -m` /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+else
+  echo "Script needs wget work"
+fi
